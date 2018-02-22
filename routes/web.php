10e2df4716ce2110de5ebb3960f2cba5622a2043
linhlt311 <linhlt@haposoft.com>
2018-02-22 17:41:31 +0700

    Admin:log in, log out

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,13 @@ Route::get('/', function () {
 
 Route::prefix('admin')->group(function() {
    Route::get('login', 'Auth\AdminController@loginForm')->name('admin.login-form');
+   Route::post('login', 'Auth\AdminController@login')->name('admin.login');
+   Route::middleware(['admin.auth'])->group(function() {
+       Route::post('logout', 'Auth\AdminController@logout')->name('admin.logout');
+       Route::get('/', 'AdminController@index')->name('admin.index');
+   });
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
