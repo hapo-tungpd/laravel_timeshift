81180c7fa55741a6638fe6e4fc8aa92c38bdae60
tungphandoan <35439300+tungphandoan@users.noreply.github.com>
2018-03-11 04:57:25 +0700

    fix validate change password

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'status' => '[1] Your current password does not matches with the password you provided. Please try again.
+                |[2] New Password cannot be same as your current password. Please choose a different password.
+                |[3] New Password cannot be same as your New password confirmation. Please choose a different password.!
+                |[4] Password changed successfully !',
+];
--- resources/views/user/usermanage/changepassword.blade.php
+++ resources/views/user/usermanage/changepassword.blade.php
@@ -34,30 +34,32 @@
                                     @endif
                                 </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('new-password') ? 'has-error' : '' }}">
                                 <label for="new-password" class="col-md-4 control-label">
                                     New Password
                                 </label>
                                 <div class="col-md-6">
                                     <input id="new-password" type="password" class="form-control" name="new-password" required>
-                                    @if ($errors->has('new-passord'))
+                                    @if ($errors->has('new-password'))
                                         <span class="help-block">
                                             <strong>{{ $errors->first('new-password') }}</strong>
                                         </span>
                                     @endif
                                 </div>
                             </div>
-
                             <div class="form-group">
                                 <label for="new-password-confirm" class="col-md-4 control-label">
                                     Confirm New Password
                                 </label>
                                 <div class="col-md-6">
                                     <input id="new-password-confirm" type="password" class="form-control" name="new-password-confirmation" required>
+                                    @if ($errors->has('new-password-confirmation'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('new-password-confirmation') }}</strong>
+                                        </span>
+                                    @endif
                                 </div>
                             </div>
-
                             <div class="form-group horizontal">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
