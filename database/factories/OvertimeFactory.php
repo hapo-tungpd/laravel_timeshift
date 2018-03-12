f44e6ed1c0b61f91146849186106b4e9bc80ea89
linhlt311 <linhlt@haposoft.com>
2018-03-12 16:51:49 +0700

    AutoFix 1

--- database/factories/OvertimeFactory.php
+++ database/factories/OvertimeFactory.php
@@ -5,7 +5,7 @@ use Carbon\Carbon;
 
 $factory->define(\App\Models\Overtime::class, function (Faker $faker) {
     $overtimeDate = $faker->dateTimeThisYear($max = 'now', $timezone = null)->format('Y-m-d');
-    $startTime = Carbon::createFromFormat('Y-m-d H:i:s',$overtimeDate . ' 18:00:00');
+    $startTime = Carbon::createFromFormat('Y-m-d H:i:s', $overtimeDate . ' 18:00:00');
     $endTime = Carbon::createFromFormat('Y-m-d H:i:s', $faker->dateTimeBetween($startTime->toDateTimeString(), $overtimeDate . ' 22:00:00')->format('Y-m-d H:i:s'));
     return [
         'user_id' => function () {
